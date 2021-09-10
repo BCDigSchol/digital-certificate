ca6ede0981aae8cba391fff15413da39cb1b549f
Matt <mnaglak@umich.edu>
2021-09-10 15:05:03 -0400

    david rumsey test map

---
+++ maps/pre1800.js
@@ -0,0 +1,40 @@
+
+//Define map start up options, here defined to center on Italy
+		var mapOptions = {
+			center: [41.8875, 12.72], //set center
+			zoom: 3 , //set initial zoom
+			maxZoom : 12,  //set max zoom
+			minZoom : 1,
+			maxBounds: [ [-90, -180] , [90,180] ]
+			}
+
+//Creates Map according to map options
+		var map = new L.map('map', mapOptions);
+
+
+//Examples of an externally called tiled basemap
+		var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+			attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+			}).addTo(map);
+
+
+//Example of a localled called tiled basemap created from a .geotiff  using gdal2tiles (workflow available)
+			var mapTest = L.tileLayer('https://maps.georeferencer.com/georeferences/bb2ea065-7ee6-55fa-a29e-1ccb6cc568a9/2020-12-06T14:11:56.847793Z/map/{z}/{x}/{y}.png?key=rFFmpdCwWU8gzjF5Xbgk', {tms: true, attribution: "", minZoom: 0, maxZoom: 10}).addTo(map);
+
+			var baseLayers = {
+				"Satellite Imagery" : Esri_WorldImagery,
+				};
+
+			var overlayMaps = {
+				"Test Map" : mapTest,
+
+				};
+				L.control.layers(baseLayers, overlayMaps).addTo(map);
+
+//Lets you see lat/long in the console window. Useful for placing non-georeferenced maps in the correct location or for placing markers
+			map.on('click', function(e){
+			var coord = e.latlng;
+			var lat = coord.lat;
+			var lng = coord.lng;
+			console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
+			});
