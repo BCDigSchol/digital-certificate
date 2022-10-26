3687af3d1bdbd8e9f388b329335c96ce522261c8
brlehman <72272753+brlehman@users.noreply.github.com>
2022-10-26 16:36:50 -0400

    Update newspaper access information

--- bcnewspaperaccess/bcnewspapers/bcnewsaccess.js
+++ bcnewspaperaccess/bcnewspapers/bcnewsaccess.js
@@ -15,7 +15,8 @@
 //Examples of an externally called tiled basemap
 		var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 			attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-			}).addTo(map);
+			})
+			.addTo(map);
 
 
 //Example of a localled called tiled basemap created from a .geotiff  using gdal2tiles (workflow available)
