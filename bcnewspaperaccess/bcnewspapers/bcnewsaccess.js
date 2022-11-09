c6386acbd8af66079eaaa4ff67403e55722b3024
brlehman <72272753+brlehman@users.noreply.github.com>
2022-11-09 11:09:44 -0500

    uploading news

--- bcnewspaperaccess/bcnewspapers/bcnewsaccess.js
+++ bcnewspaperaccess/bcnewspapers/bcnewsaccess.js
@@ -10,14 +10,12 @@
 			tap: false
 			}
 
-//Creates Map according to map options
-		var map = new L.map('map', mapOptions);
-//Examples of an externally called tiled basemap
-		var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-			attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-			})
-			.addTo(map);
-
+			//Creates Map according to map options
+					var map = new L.map('map', mapOptions);
+			//Examples of an externally called tiled basemap
+					var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+						attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+						}).addTo(map);
 
 //Example of a localled called tiled basemap created from a .geotiff  using gdal2tiles (workflow available)
 var piriReis1554 = L.tileLayer('WorldMap2/{z}/{x}/{y}.png', {tms: true, attribution: "David Rumsey Map Collection"});
